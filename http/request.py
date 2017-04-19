21375d7b04d77ad95630db6810dfd5e81de788cc
OndraK <ondrej.kajinek@gmail.com>
2017-04-19 21:40:57 +0200

    Example of environment removed from Request constructor

--- http/request.py
+++ http/request.py
@@ -20,47 +20,6 @@ class Request(object):
     )
 
     def __init__(self, environment):
-        # env
-        "http://localhost:7846/test/testovič?param=a"
-        {
-            'SCRIPT_NAME': '',
-            'REQUEST_METHOD': 'GET',
-            'PATH_INFO': '/test/testovi\xc4\x8d',
-            'SERVER_PROTOCOL': 'HTTP/1.1',
-            'QUERY_STRING': 'param=a',
-            'HTTP_USER_AGENT': (
-                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                '(KHTML, like Gecko) Chrome/57.0.2987.111 Safari/537.36 '
-                'Vivaldi/1.8.770.38'
-            ),
-            'HTTP_CONNECTION': 'keep-alive',
-            'SERVER_NAME': 'DellInspiron',
-            'REMOTE_ADDR': '127.0.0.1',
-            'wsgi.url_scheme': 'http',
-            'SERVER_PORT': '7846',
-            'uwsgi.node': 'DellInspiron',
-            'wsgi.input': "<uwsgi._Input object at 0x7f2b860392b8>",
-            'HTTP_DNT': '1',
-            'HTTP_HOST': 'localhost:7846',
-            'wsgi.multithread': False,
-            'HTTP_UPGRADE_INSECURE_REQUESTS': '1',
-            'REQUEST_URI': '/test/testovi%C4%8D?param=a',
-            'HTTP_ACCEPT': (
-                'text/html,application/xhtml+xml,application/xml;q=0.9,'
-                'image/webp,*/*;q=0.8'
-            ),
-            'wsgi.version': (1, 0),
-            'wsgi.run_once': False,
-            'wsgi.errors': (
-                "<open file 'wsgi_errors', mode 'w' at 0x7f2b7da3ded0>"
-            ),
-            'wsgi.multiprocess': True,
-            'HTTP_ACCEPT_LANGUAGE': 'cs-CZ,cs;q=0.8',
-            'uwsgi.version': '2.0.13',
-            'wsgi.file_wrapper': "<built-in function uwsgi_sendfile>",
-            'HTTP_ACCEPT_ENCODING': 'gzip, deflate, sdch, br'
-        }
-
         self.set_route_params()
         self._parse_headers(environment)
         self._save_environment(environment)
