99330c0ea72f15c137cbab84159201d15d05ace8
OndraK <ondrej.kajinek@gmail.com>
2017-04-19 22:02:05 +0200

    Simplified DependencyContainer implementation, removed duplicate methods; DC now has dict-like behaviour

--- components/dependency_injector.py
+++ components/dependency_injector.py
@@ -23,31 +23,13 @@ class DependencyContainer(object):
         super(DependencyContainer, self).__setattr__("_components", {})
 
     def __delattr__(self, key):
-        self.unregister(key)
+        log.debug("Deleting component %r", key)
+        del self._components[key]
 
-    def __getattr__(self, key):
-        return self.resolve(key)
-
-    def __setattr__(self, key, value):
-        self.register(key, value)
-
-    def get(self, key, *args, **kwargs):
-        try:
-            return self.resolve(key)
-        except UnknownComponent:
-            if len(args):
-                return args[0]
-            else:
-                return kwargs.get("default")
-
-    def register(self, key, value):
-        log.debug("Registering component %r", key)
-        self._components[key] = value
-
-    def registered(self, key):
+    def __hasattr__(self, key):
         return key in self
 
-    def resolve(self, key):
+    def __getattr__(self, key):
         log.debug("Looking for component %r", key)
         try:
             data = self._components[key]
@@ -55,15 +37,24 @@ class DependencyContainer(object):
         except KeyError:
             raise UnknownComponent(key)
 
+    def __setattr__(self, key, value):
+        log.debug("Registering component %r", key)
+        self._components[key] = value
+
+    def get(self, key, *args, **kwargs):
+        try:
+            return self.__getattr__(key)
+        except UnknownComponent:
+            if len(args):
+                return args[0]
+            else:
+                return kwargs.get("default")
+
     def singleton(self, key, value):
         def singleton_closure(dic):
-            singleton = value(self) if hasattr(value, "__call__") else value
-            self.register(key, singleton)
+            singleton = value(dic) if hasattr(value, "__call__") else value
+            dic.__setattr__(key, singleton)
             return singleton
 
         log.debug("Registering singleton %r", key)
-        self.register(key, singleton_closure)
-
-    def unregister(self, key):
-        log.debug("Deleting component %r", key)
-        del self._components[key]
+        self.__setattr__(key, singleton_closure)
