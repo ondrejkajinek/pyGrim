ac7f14e7d1315b128c518952c2b4666d92cd6985
OndraK <ondrej.kajinek@gmail.com>
2017-05-02 15:56:33 +0200

    pep

--- pygrim/__init__.py
+++ pygrim/__init__.py
@@ -1,6 +1,8 @@
 # coding: utf8
 
-from .decorators import error_method, method, not_found_method, template_method, uses_data
+from .decorators import (
+    error_method, method, not_found_method, template_method, uses_data
+)
 from .components.route import Route, RouteGroup
 from .router_exceptions import RouteSuccessfullyDispatched, RoutePassed
 from .server import Server
