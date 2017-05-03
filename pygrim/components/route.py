ondrejkajinek/pyGrim

commit f6dffcd95066bbf9d2e85c9b50b68681a13166bb

Simplified code for Route._uri_matches

pygrim/components/route.py
@@ -123,11 +123,12 @@ class Route(object):
         if self._is_regex:
             matches = self._pattern.match(uri)
             match = matches is not None
-            if match:
-                context.set_route_params(matches.groupdict())
+            route_params = matches.groupdict()
         else:
             match = self._pattern == uri
-            if match:
-                context.set_route_params({})
+            route_params = {}
+
+        if match:
+            context.set_route_params(route_params)
 
         return match

commit 64efa82bb6e239a27d5c2990943f5c740dd455f3

Fixed an issue introduced in f6dffcd95066bbf9d2e85c9b50b68681a13166bb

pygrim/components/route.py
@@ -120,7 +120,7 @@ class Route(object):
         if self.is_regex():
             matches = self._pattern.match(uri)
             match = matches is not None
-            route_params = matches.groupdict()
+            route_params = matches.groupdict() if matches else {}
         else:
             match = self._pattern == uri
             route_params = {}