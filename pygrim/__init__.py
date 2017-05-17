fd23204237d56e1f9a6da3cb9ea385a925df96e9
OndraK <ondrej.kajinek@gmail.com>
2017-05-17 17:34:02 +0200

    Removed backwards compat import -- provided backwards compat, but broke exceptions catching

--- pygrim/__init__.py
+++ pygrim/__init__.py
@@ -4,7 +4,6 @@ from . import decorators, server
 
 # compat, remove asap
 from .components.routing import Route, RouteGroup
-from .components.routing import exceptions as router_exceptions
 from .components import session
 from .decorators import (
     error_method, method, not_found_method, template_display, template_method,
