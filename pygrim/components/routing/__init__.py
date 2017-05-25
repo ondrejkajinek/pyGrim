fb87e82dabaf9bf457d905f2890c523f7b60afd2
OndraK <ondrej.kajinek@gmail.com>
2017-05-25 13:21:08 +0200

    AbstractRouter moved to separate file

--- pygrim/components/routing/__init__.py
+++ pygrim/components/routing/__init__.py
@@ -1,7 +1,8 @@
 # coding: utf8
 
+from .abstract_router import AbstractRouter
 from .exceptions import (
     DispatchFinished, RouteNotFound, RouteNotRegistered, RoutePassed
 )
 from .route import Route, RouteGroup
-from .router import AbstractRouter, Router
+from .router import Router
---
+++ pygrim/components/routing/abstract_router.py
@@ -0,0 +1,7 @@
+# coding: utf8
+
+
+class AbstractRouter(object):
+
+    def __init__(self):
+        raise NotImplementedError()
