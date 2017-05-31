75b8b0171f35eb66348f1e5cf46a6c79692ae84e
OndraK <ondrej.kajinek@gmail.com>
2017-05-31 17:43:57 +0200

    YamlConfig now prefers BaseLoader to Loader (expands only Python types); YamlConfig uses modified BaseLoader so it can handle configuration with multiple keys at once -- they turn the value into list

--- pygrim/components/config/yaml_config.py
+++ pygrim/components/config/yaml_config.py
@@ -8,6 +8,42 @@ except ImportError:
     uwsgi_log = print
 
 from yaml import load as yaml_load, parser as yaml_parser
+from yaml import BaseLoader, MappingNode
+from yaml.constructor import ConstructorError
+
+
+class PygrimYamlLoader(BaseLoader):
+
+    def construct_mapping(self, node, deep=False):
+        if not isinstance(node, MappingNode):
+            raise ConstructorError(
+                None,
+                None,
+                "expected a mapping node, but found %s" % (
+                    node.id, node.start_mark
+                )
+            )
+        mapping = {}
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            try:
+                hash(key)
+            except TypeError, exc:
+                raise ConstructorError(
+                    "while constructing a mapping",
+                    node.start_mark,
+                    "found unacceptable key (%s)" % exc,
+                    key_node.start_mark
+                )
+            value = self.construct_object(value_node, deep=deep)
+            if key in mapping:
+                if isinstance(mapping[key], list):
+                    mapping[key].append(value)
+                else:
+                    mapping[key] = [mapping[key], value]
+            else:
+                mapping[key] = value
+        return mapping
 
 
 class YamlConfig(AbstractConfig):
@@ -18,7 +54,7 @@ class YamlConfig(AbstractConfig):
         try:
             with open(path, "rb") as conf_in:
                 yaml_string = conf_in.read()
-                config = yaml_load(yaml_string)
+                config = yaml_load(yaml_string, Loader=PygrimYamlLoader)
         except yaml_parser.ParserError as exc:
             uwsgi_log("Error when parsing file %r:\n\t%s" % (path, exc))
             config = {}
