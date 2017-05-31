c0da84035cb3951ca7dc3eafb25ca563a2539c6f
OndraK <ondrej.kajinek@gmail.com>
2017-05-31 13:58:42 +0200

    Example improved, contains examples for decorators and how to properly set response status, body

--- pygrim/__init__.py
+++ pygrim/__init__.py
@@ -6,7 +6,7 @@ from . import decorators, server
 from .components.routing import Route, RouteGroup
 from .components import session
 from .decorators import (
-    error_method, method, not_found_method, template_display, template_method,
-    uses_data, error_handler
+    error_handler, error_method, method, not_found_method,
+    template_display, template_method, uses_data, error_handler
 )
 from .server import Server
