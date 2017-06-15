1d12e8e966a9585ac31e610fbdd141509bd9d7f7
OndraK <ondrej.kajinek@gmail.com>
2017-06-15 09:56:32 +0200

    Implemented c_locale decorator for internal usage -- runs method using C locale, then switches back to original locale

--- pygrim/components/session/redis_session_storage.py
+++ pygrim/components/session/redis_session_storage.py
@@ -3,6 +3,7 @@
 from .session import Session
 from .session_storage import SessionStorage
 from ..connectors import connect_redis, connect_redis_sentinel
+from ..decorators import c_locale
 from .session_exceptions import (
     # SessionInitializeError,
     SessionSaveError, SessionLoadError
@@ -22,10 +23,12 @@ class RedisSessionStorageBase(SessionStorage):
         super(RedisSessionStorageBase, self).__init__(config)
         self.redis = None
 
+    @c_locale
     def delete(self, session):
         self.redis.delete(session.get_id())
         return True
 
+    @c_locale
     def load(self, request):
         session_id, session_new = self._get_id(request)
         try:
@@ -40,6 +43,7 @@ class RedisSessionStorageBase(SessionStorage):
         else:
             return Session(session_id, session, session_new)
 
+    @c_locale
     def save(self, session):
         try:
             ret = self.redis.setex(
