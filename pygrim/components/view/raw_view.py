d9dcd9b81ed740e35ff21940dc99706a0b020437
OndraK <ondrej.kajinek@gmail.com>
2017-06-16 12:09:43 +0200

    Fixed RawView constructor

--- pygrim/components/view/raw_view.py
+++ pygrim/components/view/raw_view.py
@@ -5,5 +5,8 @@ from .abstract_view import AbstractView
 
 class RawView(AbstractView):
 
+    def __init__(self, config, extra_functions):
+        pass
+
     def render(self, context):
         return context.get_response_body()
