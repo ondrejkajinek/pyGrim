45c4299260b15fe44095df50c31fde2ed6b33703
OndraK <ondrej.kajinek@gmail.com>
2017-09-20 16:19:07 +0200

    Jinja prefix_extends implemented -- it beheves similarly to prefix_includes, but extends

--- pygrim/components/jinja_ext/utils.py
+++ pygrim/components/jinja_ext/utils.py
@@ -1,12 +1,14 @@
 # coding: utf8
 
-from jinja2.nodes import Tuple
+from jinja2.nodes import List, Tuple
 
 
-def ensure_jinja_tuple(value):
-    if isinstance(value, Tuple):
-        result = value.items
+def ensure_jinja_iterable(value):
+    if isinstance(value, (List, Tuple)):
+        result = value
+    elif isinstance(value, (tuple, list, xrange, set)):
+        result = List(value)
     else:
-        result = (value,)
+        result = List((value,))
 
     return result
