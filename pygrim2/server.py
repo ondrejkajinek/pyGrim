ondrejkajinek/pyGrim

commit ff7ffbbf73c07eb5599b630e45c3dbeaa6f725af

When route was found and exception was raised during template rendering, error handlers can still be used to handle error state

pygrim2/server.py
@@ -354,6 +354,8 @@ class Server(object):
                     self._default_not_found_handler(context)
                 else:
                     self._handle_not_found(context=context, exc=exc)
+            else:
+                self._prepare_output(context)
         except StopDispatch:
             pass
         except BaseException:
@@ -368,8 +370,6 @@ class Server(object):
                 pass
             else:
                 self._prepare_output(context)
-        else:
-            self._prepare_output(context)
 
     def _initialize_basic_components(self):
         self._load_config()

commit bfc1e0fa76bd62008ed85d74361e304cc628b9f5

Fixed bug introduced in ff7ffbbf73c07eb5599b630e45c3dbeaa6f725af

pygrim2/server.py
@@ -354,6 +354,8 @@ class Server(object):
                     self._default_not_found_handler(context)
                 else:
                     self._handle_not_found(context=context, exc=exc)
+
+                self._prepare_output(context)
             else:
                 self._prepare_output(context)
         except StopDispatch:
@@ -391,7 +393,7 @@ class Server(object):
     def _matching_error_handlers(self, request_uri, exc_mro):
         for exc in exc_mro:
             for prefix, err_cls, handler in self._error_handlers:
-                if request_uri.startswith(prefix) and exc is err_cls:
+                if request_uri.startswith(prefix) and exc == err_cls:
                     yield handler
 
     def _prepare_output(self, context):