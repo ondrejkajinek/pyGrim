37f3da2722a4f4565d68fb7b7607691f2b66ff62
OndraK <ondrej.kajinek@gmail.com>
2019-02-19 10:42:25 +0100

    python3: http codes

--- pygrim2/http/codes.py
+++ pygrim2/http/codes.py
@@ -1,16 +1,12 @@
 # coding: utf8
 
 # std
-import sys
-if sys.version_info.major == 3:
-    import http.server
-    http_responses = {
-        code: code_desc[0]
-        for code, code_desc
-        in http.server.BaseHTTPRequestHandler.responses.items()
-    }
-else:
-    from httplib import responses as http_responses
+import http.client
+http_responses = {
+    code.value: description
+    for code, description
+    in http.client.responses.items()
+}
 
 # in order of http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 http_responses.setdefault(418, "I'm a teapot")  # LOL
