ondrejkajinek/pyGrim

commit 2b5be5c89029671c85851e7d6a779a025a29000f

minor simplification of try-except block

pygrim2/server.py
@@ -382,9 +382,7 @@ class Server(object):
                     self._default_not_found_handler(context=context, exc=exc)
                 else:
                     self._handle_not_found(context=context, exc=exc)
-
-                self._prepare_output(context)
-            else:
+            finally:
                 self._prepare_output(context)
         except StopDispatch:
             pass

commit e1c9154d71455437022b1e48575511f467853956

revert of 2b5be5c89029671c85851e7d6a779a025a29000f which introduced bugs when StopDispatch was raised

pygrim2/server.py
@@ -380,7 +380,9 @@ class Server(object):
                     self._default_not_found_handler(context=context, exc=exc)
                 else:
                     self._handle_not_found(context=context, exc=exc)
-            finally:
+
+                self._prepare_output(context)
+            else:
                 self._prepare_output(context)
         except StopDispatch:
             pass