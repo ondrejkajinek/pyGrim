d6f4377d0fa4a8e3de627b72a07fb078fe151957
OndraK <ondrej.kajinek@gmail.com>
2019-03-21 15:52:42 +0100

    Removed obsolete code from Router.py -- Routes are now mapped with RouteGroups prepended

--- pygrim2/components/routing/router.py
+++ pygrim2/components/routing/router.py
@@ -1,11 +1,8 @@
-# std
-from re import compile as re_compile
-
 # local
 from .abstract_router import AbstractRouter
 from .exceptions import PatternAlreadyExists
 from .exceptions import RouteAlreadyExists, RouteNotRegistered
-from .route import NoRoute, Route, RouteGroup
+from .route import NoRoute, Route
 
 
 class Router(AbstractRouter):
@@ -13,7 +10,6 @@ class Router(AbstractRouter):
     def __init__(self):
         self._named_routes = {}
         self._routes = []
-        self._route_groups = []
 
     def get_routes(self):
         return self._routes
@@ -22,43 +18,30 @@ class Router(AbstractRouter):
         return bool(self._routes)
 
     def map(self, route):
-        if isinstance(route, Route):
-            route_name = route.get_name()
-            if route_name in self._named_routes:
-                raise RouteAlreadyExists(route)
+        if not isinstance(route, Route):
+            raise TypeError("Router can map only Route instance, %s given" % (
+                type(route)
+            ))
 
-            full_pattern = self._join(
-                self._group_pattern(), route.get_pattern().strip("/")
-            )
-            route.set_pattern(
-                re_compile("^" + full_pattern.lstrip("^"))
-                if route.is_regular() or self._is_group_regular()
-                else full_pattern
-            )
+        route_name = route.get_name()
+        if route_name in self._named_routes:
+            raise RouteAlreadyExists(route)
 
-            if any(filter(lambda existing: existing == route, self._routes)):
-                raise PatternAlreadyExists(full_pattern)
+        if any(filter(lambda existing: existing == route, self._routes)):
+            raise PatternAlreadyExists(route.get_pattern())
 
-            specificity = route.specificity()
-            index = next(
-                (
-                    i
-                    for i, r
-                    in enumerate(self._routes)
-                    if r.specificity() < specificity
-                ),
-                len(self._routes)
-            )
-            self._routes.insert(index, route)
-            self._named_routes[route_name] = route
-        elif isinstance(route, RouteGroup):
-            self.push_group(route)
-            for one in route:
-                self.map(one)
-
-            self.pop_group()
-        else:
-            raise ValueError("Unknown type: %s to map" % (type(route),))
+        specificity = route.specificity()
+        index = next(
+            (
+                i
+                for i, r
+                in enumerate(self._routes)
+                if r.specificity() < specificity
+            ),
+            len(self._routes)
+        )
+        self._routes.insert(index, route)
+        self._named_routes[route_name] = route
 
     def matching_routes(self, context):
         for route in self._routes:
@@ -70,15 +53,6 @@ class Router(AbstractRouter):
             context.set_route_params()
             context.session_changed = False
 
-    def pop_group(self):
-        try:
-            self._route_groups.pop()
-        except IndexError:
-            pass
-
-    def push_group(self, group):
-        self._route_groups.append(group)
-
     def url_for(self, route_name, params):
         try:
             route = self._named_routes[route_name]
@@ -86,30 +60,3 @@ class Router(AbstractRouter):
             raise RouteNotRegistered(route_name)
         else:
             return route.url_for(params)
-
-    def _group_pattern(self):
-        pattern = (
-            self._join(*tuple(
-                group.get_pattern()
-                for group
-                in self._route_groups
-            ))
-            if self._route_groups
-            else ""
-        )
-        return pattern
-
-    def _is_group_regular(self):
-        return any(
-            group.is_regular()
-            for group
-            in self._route_groups
-        )
-
-    def _join(self, *args):
-        return "".join(
-            part if part.lstrip("(").startswith("/") else "/" + part
-            for part
-            in args
-            if part
-        )
