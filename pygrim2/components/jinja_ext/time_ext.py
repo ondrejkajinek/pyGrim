b9b280abd275c48fc237d2065aedc9f85b449b16
OndraK <root@ondrak9.ats>
2019-06-13 12:40:07 +0200

    jinja filter to_date -- parses date string / timestamp into Datetime object

--- pygrim2/components/jinja_ext/time_ext.py
+++ pygrim2/components/jinja_ext/time_ext.py
@@ -22,26 +22,22 @@ class TimeExtension(Extension):
         environment.globals.update(self._get_functions())
 
     def as_date(self, date_, format_str=None):
+        """
+        This filter is deprecated and will be removed in the future
+        Use date_format instead
+        """
         if isinstance(date_, str):
             date_ = parse_dt(date_)
 
         return date_.strftime(format_str) if format_str else date_.isoformat()
 
-    def date_format(self, source, format_str):
-        if isinstance(source, str):
-            obj = (
-                datetime.fromtimestamp(float(source))
-                if source.isdigit()
-                else parse_dt(source)
-            )
-        elif isinstance(source, (int, float)):
-            obj = datetime.fromtimestamp(source)
-        elif isinstance(source, DTS):
-            obj = source
-        else:
-            return None
-
-        return obj.strftime(format_str)
+    def date_format(self, source, format_str=None):
+        obj = self._parse_date(source)
+        return (
+            obj.strftime(format_str) if format_str else obj.isoformat()
+            if obj
+            else None
+        )
 
     def date_now(self):
         return date.today()
@@ -57,12 +53,16 @@ class TimeExtension(Extension):
         hours, minutes = divmod(minutes, 60)
         return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
+    def to_date(self, source):
+        return self._parse_date(source)
+
     def _get_filters(self):
         return {
             "as_date": self.as_date,
             "date_format": self.date_format,
             "mins_from_secs": self.minutes_from_seconds,
-            "time_from_secs": self.time_from_seconds
+            "time_from_secs": self.time_from_seconds,
+            "to_date": self.to_date
         }
 
     def _get_functions(self):
@@ -70,3 +70,19 @@ class TimeExtension(Extension):
             "date_now": self.date_now,
             "datetime_now": self.datetime_now
         }
+
+    def _parse_date(self, source):
+        if isinstance(source, str):
+            obj = (
+                datetime.fromtimestamp(float(source))
+                if source.isdigit()
+                else parse_dt(source)
+            )
+        elif isinstance(source, (int, float)):
+            obj = datetime.fromtimestamp(source)
+        elif isinstance(source, DTS):
+            obj = source
+        else:
+            obj = None
+
+        return obj
